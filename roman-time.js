b4961b2158962f4e906c7094c7349bc4a5ba8c16
bazhenova <anyusha2595@gmail.com>
2015-10-05 22:20:51 +0500

    Add solution

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,77 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+var hours = parseInt(process.argv[2]);
+var minutes = parseInt(process.argv[3]);
+var second = ['I','II','III','IV','V','VI','VII','VIII','IX'];
+var first = ['--','X','XX','XXX','XL','L'];
+var firstNumber, secondNumber, result, roman;
 
-// Немного замечательного кода и магии
+var list = [['%%%%%','%%%  %%%','%       %','%     ','    ','        '],
+            ['  %  ',' %    % ','%       %','%     ','    ','        '],
+            ['  %  ','  %  %  ','%       %','%     ',' %% ','        '],
+            ['  %  ','   %%   ',' %     % ','%     ',' %% ',' %%%%%% '],
+            ['  %  ','  %  %  ','  %   %  ','%     ','    ','        '],
+            ['  %  ',' %    % ','   % %   ','%     ',' %% ','        '],
+            ['%%%%%','%%%  %%%','    %    ','%%%%%%',' %% ','        ']];
+
+if (hours < 0 || hours >= 24 || minutes < 0 || minutes >= 60)
+    console.log('Время указано неверно');
+else
+{
+    result = '';
+    result += arabToRoman(hours) + ':';
+    result += arabToRoman(minutes);
+    console.log(result);
+    console.log('');
+    printASCII(result);
+}
+
+function arabToRoman(number)
+{
+    roman = '';
+    firstNumber = parseInt(number / 10);
+    secondNumber = number % 10;
+    if (firstNumber == 0)
+    {
+        if (secondNumber == 0)
+        {
+            roman += first[0];
+        }
+        else
+        {
+            roman += second[secondNumber-1];
+        }
+    }
+    else
+    {
+        roman += first[firstNumber];
+        if (secondNumber != 0)
+        {
+            roman += second[secondNumber-1];
+        }       
+    }
+    return roman;
+}
+
+function printASCII(str)
+{
+    var len = str.length;
+    for (k = 0; k < 7; k++)
+    {
+        var res = '';
+        for (i = 0; i < len; i++)
+        {
+            if (str[i] == 'I')
+                res += list[k][0] + ' ';
+            if (str[i] == 'X')
+                res += list[k][1] + ' ';
+            if (str[i] == 'V')
+                res += list[k][2] + ' ';
+            if (str[i] == 'L')
+                res += list[k][3] + ' ';
+            if (str[i] == ':')
+                res += list[k][4] + ' ';
+            if (str[i] == '-')
+                res += list[k][5] + ' ';
+        }
+        console.log(res);
+    }
+}
